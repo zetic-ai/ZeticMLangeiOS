b264837dcef65f3586effc72c94d882e8d82623d
doyeop-zeticai <doyeop@zetic.ai>
2025-03-13 15:09:51 +0900

    remove LFS

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZeticMLange",
-            path: "ZeticMLange.xcframework"
+            url: "https://github.com/zetic-ai/ZeticMLangeiOS/releases/download/1.0.2/ZeticMLange.xcframework.zip",
+            checksum: "3db077a8d1103609315c4c1a83b634b900af46bb3fb6b533555047875ba990d4"
         )
     ]
 )
