39acca3686b04244f8db21503c47de4c9114753e
hyunjik-zeticai <hyunjik@zetic.ai>
2026-02-03 19:19:57 +0900

    Release version 1.5.0 again

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@ import PackageDescription
 let package = Package(
   name: "ZeticMLangeiOS",
   platforms: [
-    .iOS(.v13)
+    .iOS(.v16)
   ],
   products: [
     .library(
       name: "ZeticMLange",
-      targets: ["ZeticMLangeWrapper"]
+      targets: ["ZeticMLange"]
     )
   ],
   targets: [
     .target(
       name: "ZeticMLangeWrapper",
       dependencies: [
-        .target(name: "ZeticMLangeBinary")
+        .target(name: "ZeticMLange")
       ],
       linkerSettings: [
         .linkedFramework("Accelerate")
       ]
     ),
     .binaryTarget(
-      name: "ZeticMLangeBinary",
+      name: "ZeticMLange",
       url:
         "https://github.com/zetic-ai/ZeticMLangeiOS/releases/download/1.5.0/ZeticMLange.xcframework.zip",
       checksum: "bbb4fc45f2bade95723f531823fc1c92e9d3fa29fa9e26dfd775cef9c10ae8b1"
