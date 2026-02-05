fc882463bc84512bab90110a16bb4e9b6223eede
hyunjik-zeticai <hyunjik@zetic.ai>
2026-02-05 19:30:24 +0900

    Release version 1.5.4

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     .binaryTarget(
       name: "ZeticMLange",
       url:
-        "https://github.com/zetic-ai/ZeticMLangeiOS/releases/download/1.5.3/ZeticMLange.xcframework.zip",
-      checksum: "55d793e731960ae25de893a6c0f3def59d3961de16a6725097b780ebf0beef70"
+        "https://github.com/zetic-ai/ZeticMLangeiOS/releases/download/1.5.4/ZeticMLange.xcframework.zip",
+      checksum: "f4df94e57f916daf853ea815d5d7dfeb255e6b6fe68913ab9e3f7cf1d1e2ceb3"
     )
   ]
 )
